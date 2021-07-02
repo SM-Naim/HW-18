342f400ef26cd55ec0670265ff705447eed9a3be
S M Naim <79438483+SM-Naim@users.noreply.github.com>
2021-07-02 17:32:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function textChange(){
+	document.getElementById('h2').innerHTML="Welcome to my page"
+}
+function textChangedbl(){
+	document.getElementById('id1').innerHTML="Welcome to my page"
+}
+function fontFunction(){
+    document.getElementById('id2').style.fontSize='55px';
+}
+function hideFunction(){
+    document.getElementById('id3').style.display='none';
+}
+function showFunction(){
+    document.getElementById('id4').style.display='block';
+}
